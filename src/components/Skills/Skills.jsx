4e5017190ee8624555305898d85c6d556ffa8d81
noob-ubaid <ubaidurrahman661@gmail.com>
2025-08-29 16:17:26 +0600

    added new skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Title from "../../shared/Title";
 import Container from "../../shared/Container";
 import { motion } from "framer-motion";
@@ -15,7 +14,6 @@ import {
 import {
   SiReactquery,
   SiRedux,
-  SiGreensock,
   SiExpress,
   SiMongodb,
   SiFirebase,
@@ -23,22 +21,27 @@ import {
   SiAxios,
   SiReactrouter,
   SiTailwindcss,
+  SiNextdotjs,
+  SiTypescript,
 } from "react-icons/si";
 import { fadeIn } from "../../shared/Variants";
+
 const Skills = () => {
   const skills = [
     { id: 1, skill: "HTML", icon: "FaHtml5", color: "#E34F26" },
     { id: 2, skill: "CSS", icon: "FaCss3Alt", color: "#1572B6" },
-    { id: 13, skill: "Tailwind CSS", icon: "SiTailwindcss", color: "#06B6D4" },
     { id: 3, skill: "JavaScript", icon: "FaJsSquare", color: "#F7DF1E" },
     { id: 4, skill: "React JS", icon: "FaReact", color: "#61DAFB" },
-    { id: 5, skill: "TanStack Query", icon: "SiReactquery", color: "#FF4154" },
+    { id: 5, skill: "Next JS", icon: "SiNextdotjs", color: "#fff" },
+    { id: 6, skill: "TypeScript", icon: "SiTypescript", color: "#3178C6" },
+    { id: 7, skill: "Tailwind CSS", icon: "SiTailwindcss", color: "#06B6D4" },
+    { id: 8, skill: "TanStack Query", icon: "SiReactquery", color: "#FF4154" },
     { id: 9, skill: "Express JS", icon: "SiExpress", color: "#F7F7F7" },
     { id: 10, skill: "Node JS", icon: "FaNodeJs", color: "#339933" },
     { id: 11, skill: "MongoDB", icon: "SiMongodb", color: "#47A248" },
     { id: 12, skill: "React Router", icon: "SiReactrouter", color: "#CA4245" },
-    { id: 14, skill: "Git", icon: "FaGitAlt", color: "#F05032" },
-    { id: 15, skill: "GitHub", icon: "FaGithub", color: "#FFFFFF" },
+    { id: 13, skill: "Git", icon: "FaGitAlt", color: "#F05032" },
+    { id: 14, skill: "GitHub", icon: "FaGithub", color: "#FFFFFF" },
   ];
 
   const icons = {
@@ -49,10 +52,9 @@ const Skills = () => {
     FaGitAlt,
     FaGithub,
     FaNodeJs,
-
     SiRedux,
     TbBrandFramerMotion,
-  SiReactquery,
+    SiReactquery,
     SiExpress,
     SiMongodb,
     SiReactrouter,
@@ -60,6 +62,8 @@ const Skills = () => {
     SiFirebase,
     SiJsonwebtokens,
     SiAxios,
+    SiNextdotjs,
+    SiTypescript,
   };
 
   return (
@@ -73,10 +77,9 @@ const Skills = () => {
           variants={fadeIn("down", 0.24)}
           className="text-gray-300/70 max-w-3xl mx-auto mb-8 md:mb-14 text-center"
         >
-          I not only work with these technologies but excellent in using them
-          with best practices to deliver high-quality results, I have been
-          working with all these skills to build most of the projects
+          I not only work with these technologies but excel in using them with best practices to deliver high-quality results. I have been working with all these skills to build most of the projects
         </motion.p>
+
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-content-center gap-x-4 md:gap-x-14 gap-y-16">
           {skills.map((skill, index) => {
             const IconComponent = icons[skill.icon];
