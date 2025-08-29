e55d8e9293e7170be2b42f40ec5c30fca41c2c17
noob-ubaid <ubaidurrahman661@gmail.com>
2025-08-29 15:41:25 +0600

    added new certificate

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -1,22 +1,55 @@
 import React from "react";
 import Container from "../../shared/Container";
 import Title from "../../shared/Title";
-import img from "/certificate.png";
-import pdf from "/pdf.pdf";
+import img1 from "/certificate1.png";
+import img2 from "/certificate2.png";
+import pdf1 from "/habluprogrammer.pdf";
+import pdf2 from "/programminghero.pdf";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../shared/Variants";
+const courses = [
+  {
+    id: 1,
+    img: img2,
+    title: "Complete Web Development",
+    description:
+      "Completed the Complete Web Development course at Programming Hero, gaining hands-on experience as a Full Stack Developer. I built real-world projects using HTML, CSS, Tailwind CSS, and JavaScript, and advanced into modern frameworks and technologies including React.js, Next.js, Node.js, Express.js, and MongoDB. This journey strengthened my ability to develop both frontend and backend applications, manage databases, and build fully functional, responsive, and scalable web applications with confidence.",
+    data: "Awarded certificate in August 2025",
+    skills: [
+      "HTML",
+      "CSS",
+      "Tailwind css",
+      "JavaScript",
+      "React JS",
+      "Next JS",
+      "Node JS",
+      "Express JS",
+      "MongoDB",
+      "Framer Motion",
+    ],
+    certificate: pdf2,
+  },
+  {
+    id: 2,
+    img: img1,
+    title: "The Journey of Frontend Web Development",
+    description:
+      "Completed The Journey of Frontend Web Development course at Hablu Programmer,gaining hands-on experience building responsive and user-friendly websites with HTML, CSS, and JavaScript. I strengthened my coding skills through real projects and learned essential developer tools, version control, and debugging techniques to confidently handle real-world frontend challenges.",
+    data: "Awarded certificate in December 2024",
+    skills: [
+      "HTML",
+      "CSS",
+      "Tailwind css",
+      "JavaScript",
+      "React js",
+      "React Router",
+      "Tanstack Query",
+      "Framer Motion",
+    ],
+    certificate: pdf1,
+  },
+];
 const Certificates = () => {
-  const skill = [
-    "HTML",
-    "CSS",
-    "Tailwind css",
-    "JavaScript",
-    "React js",
-    "React Router",
-    "Tanstack Query",
-    "GSAP",
-    "Framer Motion",
-  ];
   return (
     <div id="certificate">
       <Container>
@@ -33,56 +66,49 @@ const Certificates = () => {
           here is a certification I’ve proudly earned so far as part of my
           growth journey.
         </motion.p>
-        <motion.div
-          variants={fadeIn("up", 0.15)}
-          initial="hidden"
-          whileInView="show"
-          viewport={{ once: true }}
-          className="flex items-center flex-col mt-6 md:mt-10 lg:flex-row border border-main/40 p-4 md:p-8 rounded-md gap-8 md:gap-12"
-        >
-          <div className="flex-1">
-            <img
-              className="rounded-md md:h-[400px]  h-auto object-cover w-auto "
-              src={img}
-              alt=""
-            />
-          </div>
-          <div className="flex-1 flex flex-col gap-4">
-            <h2 className="text-3xl font-semibold text-gray-300">
-             Journey of Frontend Web Development
-            </h2>
-            <p className="text-gray-300/70">
-              Completed a frontend web development course at Hablu Programmer,
-              gaining hands-on experience building responsive and user-friendly
-              websites with HTML, CSS, and JavaScript. I strengthened my coding
-              skills through real projects and learned essential developer
-              tools, version control, and debugging techniques to confidently
-              handle real-world frontend challenges.
-            </p>
-            <p className="text-gray-300/70">
-              Awarded certificate in December 2024
-            </p>
-            <div className="flex items-center flex-wrap gap-4">
-              {skill.map((tech) => (
-                <span
-                  key={tech}
-                  className="text-black rounded-full cursor-pointer bg-main px-2 py-1 md:px-3 md:py-1.5 text-sm
+        {courses.map((course) => (
+          <motion.div
+            variants={fadeIn("up", 0.15)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            className="flex items-center flex-col mt-6 md:mt-10 lg:flex-row border border-main/40 p-4 md:p-8 rounded-md gap-8 md:gap-12"
+          >
+            <div className="flex-1">
+              <img
+                className="rounded-md md:h-[430px] h-auto object-cover  w-auto lg:w-[610px]"
+                src={course.img}
+                alt={course.title}
+              />
+            </div>
+            <div className="flex-1 flex flex-col gap-4">
+              <h2 className="text-3xl font-semibold text-gray-300">
+                {course.title}
+              </h2>
+              <p className="text-gray-300/70">{course.description}</p>
+              <p className="text-gray-300/70">{course.data}</p>
+              <div className="flex items-center flex-wrap gap-4">
+                {course.skills.map((tech) => (
+                  <span
+                    key={tech}
+                    className="text-black rounded-full cursor-pointer bg-main px-2 py-1 md:px-3 md:py-1.5 text-sm
                border border-transparent
                hover:border-main/40 hover:text-gray-300 hover:bg-transparent duration-300"
-                >
-                  {tech}
-                </span>
-              ))}
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <a
+                href={course.certificate}
+                className="bg-main/20 hover:bg-gray-800 mt-2 hover:text-main duration-300 flex items-center justify-center flex-row-reverse md:gap-4 gap-2 text-center w-full rounded-md md:px-4 md:py-3 px-2 py-2"
+                target="_blank"
+              >
+                View Certificate
+              </a>
             </div>
-            <a
-              href={pdf}
-              className="bg-main/20 hover:bg-gray-800 mt-2 hover:text-main duration-300 flex items-center justify-center flex-row-reverse md:gap-4 gap-2 text-center w-full rounded-md md:px-4 md:py-3 px-2 py-2"
-              target="_blank"
-            >
-              View Certificate
-            </a>
-          </div>
-        </motion.div>
+          </motion.div>
+        ))}
       </Container>
     </div>
   );
